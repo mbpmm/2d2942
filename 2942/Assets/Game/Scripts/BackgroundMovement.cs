2dc4957650fd20c4789450c641869cb2d319520d
mbpmm <marianomcr92@gmail.com>
2019-06-06 16:13:02 -0300

    Agregado movimiento del bg y la nave (WIP)

---
+++ 2942/Assets/Game/Scripts/BackgroundMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMovement : MonoBehaviour
+{
+    public float speed;
+    MeshRenderer backRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        backRenderer = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 offset = new Vector2(0, Time.time * speed);
+        backRenderer.material.mainTextureOffset = offset;
+    }
+}
---
+++ 2942/Assets/Game/Scripts/ShipController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour
+{
+    public float speed;
+    public float vertical;
+    public float horizontal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        vertical = Input.GetAxisRaw("Vertical") ;
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        transform.position += new Vector3(transform.position.x * horizontal * Time.deltaTime * -1f * speed, transform.position.y * vertical * Time.deltaTime * -1f * speed);
+    }
+}
